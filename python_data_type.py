fdaafc232c6d2742ce82e3395fd0ce2f26c0894b
BOHEE45 <98104363+BOHEE45@users.noreply.github.com>
2022-10-24 08:32:34 +0200

    파이썬 자료형 데이터
    (1)숫자형
    (2)문자형
    (3)리스트형
    (4)딕셔너리형)

---
+++ python_data_type.py
@@ -0,0 +1,40 @@
+x= "python"
+x= 123
+x= "123"
+x= [1,2,3,4]
+x= {"name":"leo","age":"32"}
+x= True
+# print(type(x))
+
+a = 1 + 1
+a = 100-1
+a = 12*12
+a = 3/4
+# print(a)
+
+a = "Act as though \nit is impossible to fail"
+#print(a)
+
+a = """
+Act as though 
+it is impossible to fail"""
+#print(a)
+
+a = '"Failure is simply the opportunity to begin again". he says."'
+a = """ "Failure is simply the opportunity to begin again". he says." """
+
+# print(a)
+
+c = "hello python!"
+#print(c[:]) # 처음부터 끝까지 :
+
+x = "TitanicJames"
+title = x[0:7]
+director = x[7:]
+#print(title, director)
+
+x = {"key1":"value1", "key2":"value2"}
+#print(x["key2"])
+
+x = set([5,2,1])   # 중복을 없애준다
+print()
